05cec26d0be907b4a0012529397ba9b8576a72ee
Arnab <occfreelance@gmail.com>
2025-07-20 17:44:38 +0530

    code for 20july

---
+++ Array/2_LC27.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let non_val_pointer = 0;
+  // variable that maintains the position where the next non-val should shift to
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != val) {
+      nums[non_val_pointer] = nums[i];
+      non_val_pointer = non_val_pointer + 1;
+    }
+  }
+  return non_val_pointer;
+};
