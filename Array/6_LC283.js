d83fe1d8dbbb172d736d61dda4fb4608d63c641b
Arnab <occfreelance@gmail.com>
2025-07-22 00:20:56 +0530

    LC 283

---
+++ Array/6_LC283.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let movePointer = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != 0) {
+      nums[movePointer] = nums[i];
+      movePointer++;
+    }
+  }
+  for (let j = movePointer; j < nums.length; j++) {
+    nums[j] = 0;
+  }
+};
+// keep moving only the non zero numbers to the front, and once that is done and loop ends
+// your movepointer will point to the place from where all zeros need to be filled
+// so write a loop from movepointer till end of array and fill zeros
