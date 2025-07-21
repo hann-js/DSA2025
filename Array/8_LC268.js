74ad1f1fab1ae699ad3a498251cce093b94089a5
Arnab <occfreelance@gmail.com>
2025-07-22 00:46:06 +0530

    LC268

---
+++ Array/8_LC268.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  let n = nums.length;
+  let sumOfN = (n * (n + 1)) / 2;
+  return sumOfN - sum;
+};
