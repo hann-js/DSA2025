bc649f015e2b9a98dde47d0336ebcdaab9c47409
Arnab <occfreelance@gmail.com>
2025-07-24 22:34:06 +0530

    merge sort

---
+++ Sort/4_MergeSort.js
@@ -0,0 +1,25 @@
+function mergeSort(arr) {
+  let n = arr.length;
+  if (n <= 1) return arr; // base case : if the array has only 1 element
+  let mid = Math.floor(n / 2);
+  let leftArray = mergeSort(arr.slice(0, mid));
+  let rightArray = mergeSort(arr.slice(mid));
+  function mergeSortedArray(left, right) {
+    // sorts 2 sorted array
+    let p1 = 0;
+    let p2 = 0;
+    let totalLen = left.length + right.length;
+    let res = [];
+    for (let i = 0; i < totalLen; i++) {
+      if (p1 >= left.length || (p2 < right.length && left[p1] > right[p2])) {
+        res.push(right[p2]);
+        p2++;
+      } else {
+        res.push(left[p1]);
+        p1++;
+      }
+    }
+    return res;
+  }
+  return mergeSortedArray(leftArray, rightArray);
+}
